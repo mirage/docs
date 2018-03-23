9ad31345fc742cf6f1acf18bfb9d8608593876c9
Anil Madhavapeddy <anil@recoil.org>
2018-03-23 16:51:16 +0000

    fix doc gen directory

--- 04-push-docs.sh
+++ 04-push-docs.sh
@@ -5,7 +5,7 @@ git clone git@github.com:mirage/docs .gh-pages --reference .
 git -C .gh-pages checkout --orphan gh-pages
 git -C .gh-pages reset
 git -C .gh-pages clean -dxf
-cp -r /home/ocaml/.opam/4.05.0/var/cache/odig/odoc .gh-pages/
+cp -r /home/ocaml/.opam/4.05.0/var/cache/odig/odoc/* .gh-pages/
 git -C .gh-pages add .
 git -C .gh-pages commit -m "Update Pages"
 git -C .gh-pages push origin gh-pages -f
