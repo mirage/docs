979bd3553222d3870d286572f02da9e3df312983
Anil Madhavapeddy <anil@recoil.org>
2018-05-03 17:17:34 +0100

    correct directory in html gen

--- 03-generate-html.sh
+++ 03-generate-html.sh
@@ -2,9 +2,9 @@
 
 . ./vars.sh
 
-rm -rf /home/ocaml/.opam/${OCAML_VERSION}/var/cache/odig/cache
+rm -rf /home/opam/.opam/${OCAML_VERSION}/var/cache/odig/cache
 opam config exec -- odig odoc -q
 
 if [ "$BUILDKITE" != "" ]; then
-  cd /home/ocaml/.opam/${OCAML_VERSION}/var/cache/odig/ && buildkite-agent artifact upload 'odoc/* odoc/**/*'
+  cd /home/opam/.opam/${OCAML_VERSION}/var/cache/odig/ && buildkite-agent artifact upload 'odoc/* odoc/**/*'
 fi
