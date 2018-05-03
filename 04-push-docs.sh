f07dca916045899f3b02ec73fafe51121d7d9dc8
Anil Madhavapeddy <anil@recoil.org>
2018-05-03 14:39:25 +0100

    add push support for docs from buildkite

--- 04-push-docs.sh
+++ 04-push-docs.sh
@@ -1,6 +1,10 @@
 #!/bin/sh -ex
 
 . ./vars.sh
+if [ "$SSH_AUTH_SOCK" != "" ]; then
+  ssh-add -D;
+  ssh-add ~/.ssh/id_rsa.mirage-docs
+fi
 rm -rf .gh-pages
 git clone git@github.com:mirage/docs .gh-pages --reference .
 git -C .gh-pages checkout --orphan gh-pages
