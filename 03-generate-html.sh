ec0953cdb49e9e22cb0d226cdd97f251fc56edb0
Anil Madhavapeddy <anil@recoil.org>
2018-05-03 16:27:08 +0100

    split out the pipeline

--- 03-generate-html.sh
+++ 03-generate-html.sh
@@ -4,3 +4,7 @@
 
 rm -rf /home/ocaml/.opam/${OCAML_VERSION}/var/cache/odig/cache
 opam config exec -- odig odoc -q
+
+if [ "$BUILDKITE" != "" ]; then
+  cd /home/ocaml/.opam/${OCAML_VERSION}/var/cache/odig/ && buildkite-agent artifact upload 'odoc/* odoc/**/*'
+fi
--- 04-push-docs.sh
+++ 04-push-docs.sh
@@ -1,16 +1,21 @@
 #!/bin/sh -ex
 
 . ./vars.sh
-if [ "$SSH_AUTH_SOCK" != "" ]; then
+if [ "$BUILDKITE" != "" ]; then
   ssh-add -D;
   ssh-add ~/.ssh/id_rsa.mirage-docs
+  buildkite-agent artifact download odoc .
+  DIR=.
+else
+  DIR=/home/ocaml/.opam/${OCAML_VERSION}/var/cache/odig
 fi
+
 rm -rf .gh-pages
 git clone git@github.com:mirage/docs .gh-pages --reference .
 git -C .gh-pages checkout --orphan gh-pages
 git -C .gh-pages reset
 git -C .gh-pages clean -dxf
-cp -r /home/ocaml/.opam/${OCAML_VERSION}/var/cache/odig/odoc/* .gh-pages/
+cp -r ${DIR}/odoc/* .gh-pages/
 git -C .gh-pages add .
 git -C .gh-pages commit -m "Update Pages"
 git -C .gh-pages push origin gh-pages -f
